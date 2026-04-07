41422ec2034ccd4f3efdd978aad006854393bfca
Le-van Tung <lethanhtung9319@gmail.com>
2026-04-07 14:51:05 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# First-AI-Agent-with-LangGraph
+
+## Trợ lý Du lịch Thông minh (TravelBuddy)
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from first-ai-agent-with-langgraph!")
+
+
+if __name__ == "__main__":
+    main()
